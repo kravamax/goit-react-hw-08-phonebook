5b51eec80ca808629859525e2d0e9dc24a8400ed
Max Kravchenko <maks.cross@gmail.com>
2022-09-11 11:37:51 +0300

    add pages login, signin

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Container from '../components/Container';
+
+const HomePage = () => {
+  return (
+    <>
+      <Container>
+        <h1>Home page! Hello!!!</h1>
+      </Container>
+    </>
+  );
+};
+
+export default HomePage;
